0cf1e649890c4135751c812d6f22f128b8aaa5b2
61studio <leoliu198261@gmail.com>
2026-05-11 16:06:48 +1200

    remove sections

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -6,7 +6,7 @@ import Image from "next/image"
 const navLinks = [
     { label: "WORK", href: "#films" },
     { label: "ABOUT", href: "#about" },
-    { label: "ARCHIVE", href: "#instagram" },
+
     { label: "INQUIRE", href: "#contact" },
   ]
 
